3b7ef9e376f42c7a34a1ac13cbe376086fc4b102
ShenQi1996 <qisam1989@gmail.com>
2022-06-03 16:04:20 -0400

    working on the landing page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,31 @@
 import Head from "next/head";
+import Link from "next/link";
+
+//Bootstrap
+import { Container, Col, Row } from "react-bootstrap";
+
+//Style
+import classes from "../styles/home.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <Container fluid className={classes.home_center}>
       <Head>
         <title>exercise tracker</title>
         <meta name="description" content="This is my own exercise tracker" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    </div>
+      <Row>
+        <Col sm={12} className="text-center">
+          <h1 className={classes.home_text}>Welcome</h1>
+        </Col>
+        <Col sm={12} className="text-center">
+          <h1 className={classes.home_text}>Let exercises easier </h1>
+        </Col>
+        <Col>
+          <Link href="/">Get Start it</Link>
+        </Col>
+      </Row>
+    </Container>
   );
 }
